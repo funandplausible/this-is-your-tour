a5ad072d1de2b49fca047c076fffd52c1d680894
Sam Phippen <samphippen@googlemail.com>
2014-02-02 18:38:04 +0100

    Faster scrolling

--- static/js/main.js
+++ static/js/main.js
@@ -252,7 +252,7 @@ $(document).ready(function() {
                     setTimeout(function() {
                         location_index++;
                         showLocationDialogue();
-                    }, 8000);
+                    }, 5000);
                     });
                 });
             });
