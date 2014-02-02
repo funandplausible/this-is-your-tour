17ca02343e1022dbb9f09dd08b3bb51429b37d85
Sam Phippen <samphippen@googlemail.com>
2014-02-02 17:57:56 +0100

    More Pauses

--- static/js/main.js
+++ static/js/main.js
@@ -252,7 +252,7 @@ $(document).ready(function() {
                     setTimeout(function() {
                         location_index++;
                         showLocationDialogue();
-                    }, 2000);
+                    }, 4000);
                     });
                 });
             });
