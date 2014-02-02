363fb47310462edadb9ce71df3adc29586e4a3f4
Sam Phippen <samphippen@googlemail.com>
2014-02-02 15:16:00 +0100

    Fix

--- yelp_requester.py
+++ yelp_requester.py
@@ -5,10 +5,12 @@ class YelpRequester:
     def find_best_thing(self, category, place):
         root_url = "http://www.yelp.co.uk/search?find_desc=" + category + "&find_loc=" + place + "&ns=1&#sortby=rating"
         print root_url
+        root_url = root_url.replace(" ", "%20")
         response = requests.get(root_url).text
         soup = BeautifulSoup(response)
         best_place = soup.findAll(attrs={"class":"biz-name"})[1]
         next_url = "http://www.yelp.co.uk/" + best_place.attrs["href"]
+        next_url = next_url.replace(" ", "%20")
         print next_url
         response = requests.get(next_url).text
         soup = BeautifulSoup(response)
